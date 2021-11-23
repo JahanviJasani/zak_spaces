b31062b3c037d877e9c19cf5364a2070b70804ea
JahanviJasani <jasanijahanvi@gmail.com>
2021-11-23 16:44:29 +0530

    about section part I

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
   // Sticky Header
 	$(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-fixed');
+      $('.header').addClass('header-fixed');
     } else {
-      $('#header').removeClass('header-fixed');
+      $('.header').removeClass('header-fixed');
     }
   });
 
